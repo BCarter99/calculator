7c5a01e606c516daaf7c56cf1b1d041145c3ab4a
Brandon Carter <brandon.d.carter99@gmail.com>
2020-12-18 16:18:02 -0500

    Changed the equals class to equals operator, in order to more efficiently access it in the code

--- scripts/script.js
+++ scripts/script.js
@@ -15,28 +15,35 @@ keys.addEventListener('click', (event) => {
     
     // Access the clicked element
     const {target} = event;
+    const {value} = target;
 
     // Check if the clicked element is a button, exits if not
     if (!target.matches('button')) {
         return;
     }
 
-    if (target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'clear':
+            resetCalculator();
+            break;
+        default:
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
-
-    if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if (target.classList.contains('all-clear')) {
-        console.log('clear', target.value);
-        return;
-    }
+    
+    updateDisplay();
+});
 
     if (target.classList.contains('sign')) {
         console.log('sign', target.value);
@@ -45,11 +52,6 @@ keys.addEventListener('click', (event) => {
 
     if (target.classList.contains('percent')) {
         console.log('percent', target.value);
-        return;
-    }
-
-    if (target.classList.contains('equals')) {
-        console.log('equals', target.value);
         return;
     }
 
@@ -71,6 +73,12 @@ function inputDigit(digit) {
 }
 
 function inputDecimal(dot) {
+if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.'
+    calculator.waitingForSecondOperand = false;
+    return
+}
+
     if (!calculator.displayValue.includes(dot)) {
         calculator.displayValue += dot;
     }
@@ -81,8 +89,19 @@ function handleOperator(nextOperator) {
     const { firstOperand, displayValue, operator } = calculator;
     const inputValue = parseFloat(displayValue);
 
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        console.log(calculator);
+        return;
+    }
+
     if (firstOperand === null && !isNaN(inputValue)) {
         calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator);
+
+        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+        calculator.firstOperand = result;
     }
 
     calculator.waitingForSecondOperand = true;
@@ -103,4 +122,12 @@ function calculate(firstOperand, secondOperand, operator) {
     }
     
     return secondOperand;
+}
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
 }
